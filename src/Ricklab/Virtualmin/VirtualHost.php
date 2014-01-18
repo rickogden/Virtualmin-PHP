0f0289c4423e46e9c5872d1f2dc6a0f3895d504c
Rick Ogden <rick@rickogden.com>
2014-01-18 16:40:27 +0000

    formatted, changed to using guzzle and added composer info.

--- src/Ricklab/Virtualmin/VirtualHost.php
+++ src/Ricklab/Virtualmin/VirtualHost.php
@@ -1,32 +1,35 @@
 <?php
 
 namespace Ricklab\Virtualmin;
-require_once 'Virtualmin.php';
+
+require_once __DIR__ . '/Virtualmin.php';
 /**
  * Virtualmin connection.
  * Requires HTTP_Request2 from paer
  * @author Rick Ogden
  */
 
-class VirtualHost {
+class VirtualHost
+{
 
     protected $serverName;
     protected $fullDetails = [];
-    
+
     /**
      *
      * @var Virtualmin
      */
     protected $virtualmin;
-    
+
     /**
-     * 
+     *
      * @param \Ricklab\Virtualmin\Virtualmin $virtualmin
      * @param string $args
      * @return \self
      * @throws \Exception
      */
-    public static function get(Virtualmin $virtualmin, $args = []) {
+    public static function get(Virtualmin $virtualmin, $args = [])
+    {
         $returnArray = $virtualmin->run('list-domains', $args);
         $hosts = [];
         if ($returnArray['status'] == 'success') {
@@ -39,45 +42,110 @@ class VirtualHost {
 
         return $hosts;
     }
-    
+
     /**
-     * 
+     *
      * @param \Ricklab\Virtualmin\Virtualmin $virtualmin
      * @param string $username
      * @return \self
      */
-    public static function getByUsername(Virtualmin $virtualmin, $username) {
+    public static function getByUsername(Virtualmin $virtualmin, $username)
+    {
         try {
             $returnArray = self::get($virtualmin, ['user' => $username]);
-            if (length($returnArray) == 1)
+            if (length($returnArray) == 1) {
                 return $returnArray[0];
-        } catch (Exception $e) {
-            return null;
-        }
-    }
-    
-    /**
-     * 
-     * @param \Ricklab\Virtualmin\Virtualmin $virtualmin
-     * @param string $domain
-     * @return \self
-     */
-    public static function getByDomain(Virtualmin $virtualmin, $domain) {
-        try {
-            $returnArray = self::get($virtualmin, ['user' => $username]);
-            if (length($returnArray) == 1)
-                return $returnArray[0];
+            }
         } catch (Exception $e) {
             return null;
         }
     }
 
-    public function __construct(Virtualmin $virtualmin, $name, $hostDetails = []) {
+    /**
+     *
+     * @param \Ricklab\Virtualmin\Virtualmin $virtualmin
+     * @param string $domain
+     * @return \self
+     */
+    public static function getByDomain(Virtualmin $virtualmin, $domain)
+    {
+        try {
+            $returnArray = self::get($virtualmin, ['domain' => $domain]);
+            if (length($returnArray) == 1) {
+                return $returnArray[0];
+            }
+        } catch (Exception $e) {
+            return null;
+        }
+    }
+
+    public function __construct(Virtualmin $virtualmin, $name, $hostDetails = [])
+    {
         $this->serverName = $name;
         $this->fullDetails = $hostDetails;
         $this->virtualmin = $virtualmin;
     }
-    
-    
+
+    public function emailUser($subject, $message)
+    {
+
+    }
+
+    /**
+     *
+     * @param string $password
+     * @return \Ricklab\Virtualmin\VirtualHost
+     * @throws \Exception
+     */
+    public function changePassword($password)
+    {
+
+        return $this->modify(['pass' => $password]);
+    }
+
+    /**
+     *
+     * @param int $quota
+     * @return \Ricklab\Virtualmin\VirtualHost
+     */
+    public function changeQuota($quota)
+    {
+        return $this->modify(['quota' => $quota]);
+    }
+
+    /**
+     *
+     * @param type $email
+     * @return \Ricklab\Virtualmin\VirtualHost
+     */
+    public function changeEmail($email)
+    {
+        return $this->modify(['email' => $email]);
+    }
+
+    /**
+     *
+     * @param mixed $parameters
+     * @return \Ricklab\Virtualmin\VirtualHost
+     * @throws \Exception
+     */
+    protected function modify($parameters = [])
+    {
+        $return = $this->virtualmin->run('modify-domain', array_merge(['domain' => $this->serverName], $parameters));
+        if ($return['status'] == 'failure') {
+            throw new \Exception($return['error']);
+        }
+
+        return $this;
+    }
+
+    public function __get($property)
+    {
+        if ($property == 'serverName') {
+            return $this->serverName;
+        } else {
+            return $this->fullDetails[$property];
+        }
+    }
 
 }
--- src/Ricklab/Virtualmin/Virtualmin.php
+++ src/Ricklab/Virtualmin/Virtualmin.php
@@ -8,80 +8,103 @@
 
 namespace Ricklab\Virtualmin;
 
-require_once 'VirtualHost.php';
+use Guzzle\Http\Client;
 
-class Virtualmin {
+require_once __DIR__ . '/VirtualHost.php';
 
-    
-    protected $username = '';
-    protected $password = '';
+class Virtualmin
+{
+
+
+    private $username = '';
+    private $password = '';
     protected $host = 'localhost';
     protected $port = 10000;
 
-    public function __construct($username = null, $password = null, $host = null, $port = null) {
-        if($username !== null) $this->username = $username;
-        if($password !== null) $this->password = $password;
-        if($host !== null) $this->host = $host;
-        if($port !== null) $this->port = $port;
+    public function __construct($username = null, $password = null, $host = null, $port = null)
+    {
+        if ($username !== null) {
+            $this->username = $username;
+        }
+        if ($password !== null) {
+            $this->password = $password;
+        }
+        if ($host !== null) {
+            $this->host = $host;
+        }
+        if ($port !== null) {
+            $this->port = $port;
+        }
     }
-    
+
     /**
-     * 
+     *
      * @param string $domain
      * @param array $options
      * @return VirtualHost
      */
-    public function registerDomain($domain, $options = []) {
+    public function registerDomain($domain, $options = [])
+    {
         $options['domain'] = $domain;
         $this->run('create-domain', $options);
         return VirtualHost::getByDomain($this, $domain);
-        
+
     }
-    
+
     /**
      * Runs a program.
-     * 
+     *
      * @param string $program
      * @param array $options
      * @return array results
      */
-    public function run($program, $options = []) {
-        
+    public function run($program, array $options = [])
+    {
+
         $options['program'] = $program;
         $options['multiline'] = '';
         $options['json'] = '1';
-        
-        $url = 'http://'.$this->host.':'.$this->port.'/virtual-server/remote.cgi';
-        $request = new HTTP_Request2($url);
+
+        $client = new Client('https://' . $this->host . ':' . $this->port);
+        $client->setSslVerification(false);
+        $request = $client->get(
+            '/virtual-server/remote.cgi',
+            [],
+            [
+                'query' => $options
+            ]
+        );
         $request->setAuth($this->username, $this->password);
-        $request->getUrl()->setQueryVariables($options);
-        
-        return json_decode($request->send()->getBody());
+
+        return json_decode($request->send()->getBody(), true);
     }
-    
+
     /**
-     * 
+     *
      * @param string $domain
      * @return \Ricklab\Virtualmin\virtualHost
      */
-    public function getVirtualHostByDomain($domain) {
+    public function getVirtualHostByDomain($domain)
+    {
         return VirtualHost::getByDomain($this, $domain);
     }
-    
+
     /**
-     * 
+     *
      * @param string $username
      * @return \Ricklab\Virtualmin\virtualHost
      */
-    public function getVirtualHostByUsername($username) {
+    public function getVirtualHostByUsername($username)
+    {
         return VirtualHost::getByUsername($this, $username);
     }
-    
+
     /**
-     * 
+     *
      * @return \Ricklab\Virtualmin\virtualHost
      */
-    public function getAllVirtualHosts() {
+    public function getAllVirtualHosts()
+    {
         return VirtualHost::get($this);
     }
 
